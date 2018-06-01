a569adf952d95acab2c10fdaca55fb91008b012c
Danielle H Gamble <dg12181@srcs.loc>
2018-06-01 12:19:23 -0700

    need to fix two out of three

--- rochambeau.js
+++ rochambeau.js
@@ -1,5 +1,8 @@
 // Stores the current player's and computer's choices
 // 0 = Rock, 1 = Paper, 2 = Scissors
+// Stores the lables for the choices
+var choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+var score = [0,0,0];
 var playerChoice;
 var computerChoice;
 // Stores the player's choice
@@ -11,17 +14,21 @@ function storePlayerChoice(choice) {
 var rockButton = document.getElementById("rock");
 var paperButton = document.getElementById("paper");
 var scissorsButton = document.getElementById("scissors");
+var lizardButton = document.getElementById("lizard");
+var spockButton = document.getElementById("spock");
 var playButton = document.getElementById("play");
 
 // Add the event handlers
 rockButton.addEventListener('click', () => {storePlayerChoice(0)});
 paperButton.addEventListener('click', () => {storePlayerChoice(1)});
 scissorsButton.addEventListener('click', () => {storePlayerChoice(2)});
+lizardButton.addEventListener('click', () => {storePlayerChoice(3)});
+spockButton.addEventListener('click', () => {storePlayerChoice(4)});
 playButton.addEventListener('click', () => {playGame()});
 
 // Generate computer's random choice
 function storeComputerChoice() {
-    computerChoice = Math.floor(Math.random()*3);
+    computerChoice = Math.floor(Math.random()*5);
     console.log("Computer choice = " + computerChoice);
 }
 // Stores the player's choice, then call's the function for storing the computer's choice
@@ -35,18 +42,55 @@ function playGame(){
     // Here is the game ruleset algorithm
     if (playerChoice == computerChoice) {
         // We have a tie!
-        console.log("tie");
-    } else if (playerChoice == 0 && computerChoice == 2) {
+        updateScore(1);
+        displayGameResult("tie")
+    } else if (playerChoice == 0 && (computerChoice == 2 || computerChoice == 3)) {
         // Rock beats scissors - a win!
-        console.log("win");
-    } else if (playerChoice == 1 && computerChoice == 0) {
+        updateScore(0);
+        displayGameResult("win")
+    } else if (playerChoice == 1 && (computerChoice == 0 || computerChoice == 4)) {
         // Paper beats scissors - a win!
-        console.log("win");
-    } else if (playerChoice == 2 && computerChoice == 1) {
+        updateScore(0);
+        displayGameResult("win")
+    } else if (playerChoice == 2 && (computerChoice == 1 || computerChoice == 3)) {
         // Scissors beats paper - a win!
-        console.log("win");
+        updateScore(0);
+        displayGameResult("win")
+    } else if (playerChoice == 3 && (computerChoice == 1 || computerChoice == 4)) {
+        // lizard beats paper or spock - a win!
+        updateScore(0);
+        displayGameResult("win")
+    } else if (playerChoice == 4 && (computerChoice == 0 || computerChoice == 2)) {
+        // spock beats scissors or rock - a win!
+        updateScore(0);
+        displayGameResult("win")
     } else {
         // All other combinations are losses
-        console.log("lose");
+        updateScore(2);
+        displayGameResult("lose")
     }
+}
+//Displays the result of the game
+function displayGameResult(result){
+    // Create a message for the player
+    var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + ".";
+    // Add to the base message if it was a win, loss, or tie
+    if (result === "win") {
+        // Display that it was a win
+        document.getElementById("result").textContent = message + " YOU WIN!";
+        document.getElementById("result").className = "alert alert-success";
+    } else if (result === "lose") {
+        // Display that it was a loss
+        document.getElementById("result").textContent = message + " YOU LOSE!";
+        document.getElementById("result").className = "alert alert-danger";
+    } else {
+        // Display that it was a tie
+        document.getElementById("result").textContent = message + " A tie.";
+        document.getElementById("result").className = "alert alert-info";
+    }
+}
+// Updates the score
+function updateScore(val){
+    ++score[val];
+    console.log("The score is now " + score);
 }
