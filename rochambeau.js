48fcd88f1e0317cd88c1bf3f7d5f9e43adabd55d
Danielle H Gamble <dg12181@srcs.loc>
2018-06-11 11:18:57 -0700

    need to match pairs

--- rochambeau.js
+++ rochambeau.js
@@ -1,14 +1,19 @@
 // Stores the current player's and computer's choices
 // 0 = Rock, 1 = Paper, 2 = Scissors
-// Stores the lables for the choices
-var choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
-var score = [0,0,0];
 var playerChoice;
 var computerChoice;
-// Stores the player's choice
+
+// Stores the lables for the choices
+var choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+
+// Variable to store the score
+// score[0] = wins, score[1] = ties, score[2] = losses
+var score = [0,0,0];
+
+// Stores the player's choice, then call's the function for storing the computer's choice
 function storePlayerChoice(choice) {
     playerChoice = choice;
-    console.log("My choice = " + playerChoice);
+    console.log("My choice = " + choice);
     storeComputerChoice();
 }
 
@@ -17,23 +22,7 @@ function storeComputerChoice() {
     computerChoice = Math.floor(Math.random()*5);
     console.log("Computer choice = " + computerChoice);
 }
-// Stores the player's choice, then call's the function for storing the computer's choice
-function storePlayerChoice(choice) {
-    playerChoice = choice;
-    console.log("My choice = " + choice);
-    storeComputerChoice();
-}
-// Updates the score
-function updateScore(val){
-    ++score[val];
-    console.log("The score is now " + score);
-}
-// Function for displaying the score
-function updateScoreBoard(){
-    document.getElementById("wins").textContent = score[0];
-    document.getElementById("losses").textContent = score[2];
-    document.getElementById("ties").textContent = score[1];
-}
+
 // This is the function for playing the game
 function playGame(){
     // Here is the game ruleset algorithm
@@ -67,8 +56,11 @@ function playGame(){
         displayGameResult("lose")
     }
 }
+
+
 //Displays the result of the game
 function displayGameResult(result){
+    // Define an array of text labels for the choices 0, 1, 2;
     // Create a message for the player
     var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + ".";
     // Add to the base message if it was a win, loss, or tie
@@ -85,6 +77,21 @@ function displayGameResult(result){
         document.getElementById("result").textContent = message + " A tie.";
         document.getElementById("result").className = "alert alert-info";
     }
+
+    updateScoreBoard();
+}
+
+// Updates the score
+function updateScore(val){
+    ++score[val];
+    console.log("The score is now " + score);
+}
+
+// Function for displaying the score
+function updateScoreBoard(){
+    document.getElementById("wins").textContent = score[0];
+    document.getElementById("losses").textContent = score[2];
+    document.getElementById("ties").textContent = score[1];
 }
 
 // The button elements
@@ -95,7 +102,6 @@ var lizardButton = document.getElementById("lizard");
 var spockButton = document.getElementById("spock");
 var playButton = document.getElementById("play");
 
-
 // Add the event handlers
 rockButton.addEventListener('click', () => {storePlayerChoice(0)});
 paperButton.addEventListener('click', () => {storePlayerChoice(1)});
@@ -103,3 +109,4 @@ scissorsButton.addEventListener('click', () => {storePlayerChoice(2)});
 lizardButton.addEventListener('click', () => {storePlayerChoice(3)});
 spockButton.addEventListener('click', () => {storePlayerChoice(4)});
 playButton.addEventListener('click', () => {playGame()});
+// This is the function for playing the game
